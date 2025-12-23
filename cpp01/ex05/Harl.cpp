bd245ed388f728cf98406a3063917dcc5de6034b
tim-daria <darya.timofeeva.97@gmail.com>
2025-12-23 12:14:00 +0100

    fix: Makefile and corrected ex06 in cpp01

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -14,23 +14,19 @@
 
 Harl::Harl(){};
 
-void Harl::debug(void)
-{
+void Harl::debug(void) {
 	std::cout << "[ DEBUG ]\n I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\n I really do!" << std::endl;
 }
 
-void Harl::info(void)
-{
+void Harl::info(void) {
 	std::cout << "[ INFO ]\n I cannot believe adding extra bacon costs more money.\n You didn't put enough bacon in my burger!\n If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void Harl::warning(void)
-{
+void Harl::warning(void) {
 	std::cout << "[ WARNING ]\n I think I deserve to have some extra bacon for free.\n I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
-void Harl::error(void)
-{
+void Harl::error(void) {
 	std::cout << "[ ERROR ]\n This is unacceptable!\n I want to speak to the manager now." << std::endl;
 }
 
@@ -45,8 +41,7 @@ void Harl::complain(std::string level)
 	};
 	for (int i = 0; i < 4; i++)
 	{
-		if (levels[i] == level)
-		{
+		if (levels[i] == level) {
 			return ((this->*functions[i])());
 		}
 	}
--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -14,43 +14,37 @@
 
 Harl::Harl(){};
 
-void Harl::debug(void)
-{
+void Harl::debug(void) {
 	std::cout << "[ DEBUG ]\n I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\n I really do!" << std::endl;
 }
 
-void Harl::info(void)
-{
+void Harl::info(void) {
 	std::cout << "[ INFO ]\n I cannot believe adding extra bacon costs more money.\n You didn't put enough bacon in my burger!\n If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
-void Harl::warning(void)
-{
+void Harl::warning(void) {
 	std::cout << "[ WARNING ]\n I think I deserve to have some extra bacon for free.\n I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
-void Harl::error(void)
-{
+void Harl::error(void) {
 	std::cout << "[ ERROR ]\n This is unacceptable!\n I want to speak to the manager now." << std::endl;
 }
 
 void Harl::complain(std::string level)
 {
-	int index;
-	if (level == "DEBUG")
-		index = 1;
-	else if (level == "INFO")
-		index = 2;
-	else if (level == "WARNING")
-		index = 3;
-	else if (level == "ERROR")
-		index = 4;
-	else
-		index = 0;
-	switch (index){
-		case 0:
-			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	int index = 0;
+
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	for (int i = 0; i < 4; i++)
+	{
+		if (levels[i] == level)
+		{
+			index = i + 1;
 			break;
+		}
+	}
+	switch (index)
+	{
 		case 1:
 			Harl::debug();
 		case 2:
@@ -60,5 +54,7 @@ void Harl::complain(std::string level)
 		case 4:
 			Harl::error();
 			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 }
